8bac3bcb28c874c119a32cfc327c4955bddeae91
CCRI-TCRI <sinclairesebastian@outlook.com>
2025-07-06 11:58:55 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css"
 export const metadata: Metadata = {
   title: "Lubiri Secondary School - Election System",
   description: "Digital voting platform for Lubiri Secondary School prefectorial elections",
-  generator: "v0.dev",
+  Developer: "Sseruwagi Sinclaire Sebastian",
+  
   icons: {
     icon: "/favicon.ico",
   },
